41f78da34d184b0a30fb4551aa44da34983d2b18
Jiacheng Huang <huangjiacheng0709@outlook.com>
2024-08-07 15:55:42 +0800

    Define the kernel functions at the top level

--- add.py
+++ add.py
@@ -4,17 +4,19 @@ import triton
 from ninetoothed import Symbol, Tensor
 
 
+BLOCK_SIZE = Symbol("BLOCK_SIZE", meta=True)
+
+
+@ninetoothed.jit
+def add_kernel(
+    lhs: Tensor(1).tile((BLOCK_SIZE,)),
+    rhs: Tensor(1).tile((BLOCK_SIZE,)),
+    output: Tensor(1).tile((BLOCK_SIZE,)),
+):
+    output = lhs + rhs  # noqa: F841
+
+
 def add(lhs, rhs):
-    BLOCK_SIZE = Symbol("BLOCK_SIZE", meta=True)
-
-    @ninetoothed.jit
-    def add_kernel(
-        lhs: Tensor(1).tile((BLOCK_SIZE,)),
-        rhs: Tensor(1).tile((BLOCK_SIZE,)),
-        output: Tensor(1).tile((BLOCK_SIZE,)),
-    ):
-        output = lhs + rhs  # noqa: F841
-
     output = torch.empty_like(lhs)
 
     add_kernel(lhs, rhs, output)
