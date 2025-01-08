b41ce00cf564e88d7bec9bfdf44ce7d4f0c6e956
Jiacheng Huang <huangjiacheng0709@outlook.com>
2025-01-08 16:45:11 +0800

    Add an example for 2D max pooling

---
+++ max_pool2d.py
@@ -0,0 +1,85 @@
+import math
+
+import ninetoothed
+import ninetoothed.language as ntl
+import torch
+import torch.nn.functional as F
+import triton
+from ninetoothed import Symbol, Tensor
+
+
+def arrangement(input, output):
+    WINDOW_HEIGHT = Symbol("WINDOW_HEIGHT", constexpr=True)
+    WINDOW_WIDTH = Symbol("WINDOW_WIDTH", constexpr=True)
+
+    return input.tile((1, 1, WINDOW_HEIGHT, WINDOW_WIDTH)), output.tile((1, 1, 1, 1))
+
+
+def application(input, output):
+    output = ntl.max(input)  # noqa: F841
+
+
+max_pool2d_kernel = ninetoothed.make(
+    arrangement, application, (Tensor(4, other=float("-inf")), Tensor(4))
+)
+
+
+def max_pool2d(input, window_shape):
+    n, c, h, w = input.shape
+    r, s = window_shape
+    p = math.ceil((h - r) / r + 1)
+    q = math.ceil((w - s) / s + 1)
+
+    output = torch.empty(n, c, p, q, dtype=input.dtype, device=input.device)
+
+    max_pool2d_kernel(input, output, WINDOW_HEIGHT=r, WINDOW_WIDTH=s)
+
+    return output
+
+
+if __name__ == "__main__":
+    torch.manual_seed(0)
+    input_shape = (32, 3, 64, 64)
+    window_shape = (3, 3)
+    input = torch.randn(input_shape, dtype=torch.float16, device="cuda")
+    ninetoothed_output = max_pool2d(input, window_shape)
+    torch_output = F.max_pool2d(input, window_shape, ceil_mode=True)
+    print(ninetoothed_output)
+    print(torch_output)
+    if torch.allclose(ninetoothed_output, torch_output):
+        print("✅ NineToothed and PyTorch match.")
+    else:
+        print("❌ NineToothed and PyTorch differ.")
+
+    @triton.testing.perf_report(
+        triton.testing.Benchmark(
+            x_names=["h", "w"],
+            x_vals=[8 * i for i in range(2, 33)],
+            line_arg="provider",
+            line_vals=["ninetoothed", "torch"],
+            line_names=["NineToothed", "PyTorch"],
+            styles=[("blue", "-"), ("green", "-")],
+            ylabel="TFLOPS",
+            plot_name="2d-max-pooling-performance",
+            args={},
+        )
+    )
+    def benchmark(h, w, provider):
+        n, c, h, w = 64, 64, h, w
+        r, s = 3, 3
+        dtype = torch.float16
+        device = "cuda"
+        input = torch.randn((n, c, h, w), dtype=dtype, device=device)
+        window_shape = (r, s)
+
+        if provider == "ninetoothed":
+            ms = triton.testing.do_bench(lambda: max_pool2d(input, window_shape))
+        elif provider == "torch":
+            ms = triton.testing.do_bench(lambda: F.max_pool2d(input, window_shape))
+
+        def gbps(ms):
+            return 2 * input.numel() * input.element_size() / ms * 1e-6
+
+        return gbps(ms)
+
+    benchmark.run(show_plots=True, print_data=True, save_path=".")
