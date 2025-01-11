79513295ba463f15d01ca137f0a2d7b2b97de66a
Jiacheng Huang <huangjiacheng0709@outlook.com>
2025-01-11 12:06:02 +0800

    Improve the performance of the 2D convolution compute kernel written in Triton

--- conv2d.py
+++ conv2d.py
@@ -23,11 +23,8 @@ def arrangement(input, filter, output):
     return matmul.arrangement(input_flattened, filter_permuted, output_flattened)
 
 
-conv2d_kernel = ninetoothed.make(
-    arrangement,
-    matmul.application,
-    (Tensor(4), Tensor(4, constexpr_shape=True), Tensor(4)),
-)
+tensors = (Tensor(4, constexpr_shape=True) for _ in range(3))
+conv2d_kernel = ninetoothed.make(arrangement, matmul.application, tensors)
 
 
 def conv2d(input, filter):
@@ -50,13 +47,13 @@ def triton_conv2d_kernel(
     input_ptr,
     filter_ptr,
     output_ptr,
-    n,
-    c,
-    h,
-    w,
-    k,
-    r,
-    s,
+    N: tl.constexpr,
+    C: tl.constexpr,
+    H: tl.constexpr,
+    W: tl.constexpr,
+    K: tl.constexpr,
+    R: tl.constexpr,
+    S: tl.constexpr,
     input_stride_n,
     input_stride_c,
     input_stride_h,
@@ -74,16 +71,16 @@ def triton_conv2d_kernel(
     BLOCK_SIZE_K: tl.constexpr,
     GROUP_SIZE_M: tl.constexpr,
 ):
-    p = h - r + 1
-    q = w - s + 1
+    P: tl.constexpr = H - R + 1
+    Q: tl.constexpr = W - S + 1
 
-    gemm_m = n * p * q
-    gemm_n = k
-    gemm_k = c * r * s
+    GEMM_M: tl.constexpr = N * P * Q
+    GEMM_N: tl.constexpr = K
+    GEMM_K: tl.constexpr = C * R * S
 
     pid = tl.program_id(0)
-    num_pid_gemm_m = tl.cdiv(gemm_m, BLOCK_SIZE_M)
-    num_pid_gemm_n = tl.cdiv(gemm_n, BLOCK_SIZE_N)
+    num_pid_gemm_m = tl.cdiv(GEMM_M, BLOCK_SIZE_M)
+    num_pid_gemm_n = tl.cdiv(GEMM_N, BLOCK_SIZE_N)
     num_pid_in_group = GROUP_SIZE_M * num_pid_gemm_n
     group_id = pid // num_pid_in_group
     first_pid_gemm_m = group_id * GROUP_SIZE_M
@@ -94,49 +91,46 @@ def triton_conv2d_kernel(
     offs_gemm_i = pid_gemm_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
     offs_gemm_j = pid_gemm_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
 
-    offs_n = offs_gemm_i // (p * q)
+    offs_n = offs_gemm_i // (P * Q)
     offs_k = offs_gemm_j
-    npq_residual = offs_gemm_i % (p * q)
-    offs_p = npq_residual // q
-    offs_q = npq_residual % q
+    npq_residual = offs_gemm_i % (P * Q)
+    offs_p = npq_residual // Q
+    offs_q = npq_residual % Q
+
+    input_offs_gemm_m = (
+        offs_n * input_stride_n + offs_p * input_stride_h + offs_q * input_stride_w
+    )[:, None]
+    filter_offs_gemm_n = (offs_k * filter_stride_k)[None, :]
 
     accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
 
-    for i in range(0, tl.cdiv(gemm_k, BLOCK_SIZE_K)):
+    for i in range(0, tl.cdiv(GEMM_K, BLOCK_SIZE_K)):
         offs_gemm_k = i * BLOCK_SIZE_K + tl.arange(0, BLOCK_SIZE_K)
 
-        offs_c = offs_gemm_k // (r * s)
-        crs_residual = offs_gemm_k % (r * s)
-        offs_r = crs_residual // s
-        offs_s = crs_residual % s
+        offs_c = offs_gemm_k // (R * S)
+        crs_residual = offs_gemm_k % (R * S)
+        offs_r = crs_residual // S
+        offs_s = crs_residual % S
 
-        offs_h = offs_p[:, None] + offs_r[None, :]
-        offs_w = offs_q[:, None] + offs_s[None, :]
+        input_offs_gemm_n = (
+            offs_c * input_stride_c + offs_r * input_stride_h + offs_s * input_stride_w
+        )[None, :]
+        input_ptrs = input_ptr + input_offs_gemm_m + input_offs_gemm_n
+        input_mask = ((offs_n < N) & (offs_p < P) & (offs_q < Q))[:, None] & (
+            (offs_c < C) & (offs_r < R) & (offs_s < S)
+        )[None, :]
+        input = tl.load(input_ptrs, mask=input_mask)
 
-        input_ptrs = (
-            input_ptr
-            + offs_n[:, None] * input_stride_n
-            + offs_c[None, :] * input_stride_c
-            + offs_h * input_stride_h
-            + offs_w * input_stride_w
-        )
-        input_mask = (
-            (offs_n[:, None] < n) & (offs_c[None, :] < c) & (offs_h < h) & (offs_w < w)
-        )
-
-        filter_ptrs = (
-            filter_ptr
-            + offs_k[None, :] * filter_stride_k
-            + offs_c[:, None] * filter_stride_c
-            + offs_r[:, None] * filter_stride_r
-            + offs_s[:, None] * filter_stride_s
-        )
-        filter_mask = (offs_k[None, :] < k) & (
-            (offs_c < c) & (offs_r < r) & (offs_s < s)
+        filter_offs_gemm_m = (
+            offs_c * filter_stride_c
+            + offs_r * filter_stride_r
+            + offs_s * filter_stride_s
         )[:, None]
-
-        input = tl.load(input_ptrs, mask=input_mask, other=0.0)
-        filter = tl.load(filter_ptrs, mask=filter_mask, other=0.0)
+        filter_ptrs = filter_ptr + filter_offs_gemm_m + filter_offs_gemm_n
+        filter_mask = (offs_k[None, :] < K) & (
+            (offs_c < C) & (offs_r < R) & (offs_s < S)
+        )[:, None]
+        filter = tl.load(filter_ptrs, mask=filter_mask)
 
         accumulator = tl.dot(input, filter, accumulator)
 
@@ -144,18 +138,19 @@ def triton_conv2d_kernel(
 
     output_ptrs = (
         output_ptr
-        + offs_n[:, None] * output_stride_n
-        + offs_k[None, :] * output_stride_k
-        + offs_p[:, None] * output_stride_p
-        + offs_q[:, None] * output_stride_q
+        + (
+            offs_n * output_stride_n
+            + offs_p * output_stride_p
+            + offs_q * output_stride_q
+        )[:, None]
+        + (offs_k * output_stride_k)[None, :]
     )
     output_mask = (
-        (offs_n[:, None] < n)
-        & (offs_k[None, :] < k)
-        & (offs_p[:, None] < p)
-        & (offs_q[:, None] < q)
+        (offs_n[:, None] < N)
+        & (offs_k[None, :] < K)
+        & (offs_p[:, None] < P)
+        & (offs_q[:, None] < Q)
     )
-
     tl.store(output_ptrs, output, mask=output_mask)
 
 
