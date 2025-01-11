83f7596e70510a503b1660c32786bb3826cb7c0d
Jiacheng Huang <huangjiacheng0709@outlook.com>
2025-01-12 00:10:21 +0800

    Add `rms_norm` data into `code_size_comparison.py`

--- code_size_comparison.py
+++ code_size_comparison.py
@@ -6,11 +6,14 @@ plt.rcParams["font.family"] = "JetBrains Mono"
 plt.rcParams["font.weight"] = "bold"
 plt.rcParams["axes.labelweight"] = "bold"
 
-kernels = ("add", "softmax", "matmul", "conv2d", "attention")
-lines_of_code = {"Triton": (19, 25, 57, 110, 98), "NineToothed": (10, 12, 34, 17, 51)}
+kernels = ("add", "softmax", "rms_norm", "matmul", "conv2d", "attention")
+lines_of_code = {
+    "Triton": (19, 25, 21, 57, 110, 98),
+    "NineToothed": (10, 12, 13, 34, 17, 51),
+}
 
 x = np.arange(len(kernels))
-width = 0.4
+width = 0.25
 multiplier = 0
 
 fig, ax = plt.subplots()
@@ -18,15 +21,15 @@ fig, ax = plt.subplots()
 for provider, lines in lines_of_code.items():
     offset = width * multiplier
     rects = ax.bar(x + offset, lines, width, label=provider)
-    ax.bar_label(rects, fontsize=16)
+    ax.bar_label(rects, fontsize=12)
     multiplier += 1
 
-ax.set_ylabel("Lines of Code", fontsize=16)
-ax.tick_params(axis="y", labelsize=12, labelcolor="gray")
-ax.set_xticks(x + width / 2, kernels, fontsize=12)
+ax.set_ylabel("Lines of Code", fontsize=12)
+ax.tick_params(axis="y", labelsize=10, labelcolor="gray")
+ax.set_xticks(x + width / 2, kernels, fontsize=10)
 ax.xaxis.set_ticks_position("none")
 ax.yaxis.set_ticks_position("none")
-ax.legend(fontsize=10)
+ax.legend(ncols=2, fontsize=10)
 ax.spines[["top", "left", "right"]].set_visible(False)
 ax.spines["bottom"].set_linewidth(1.5)
 ax.grid(axis="y", linewidth=1.5)
