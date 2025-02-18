7c3e69d30994562de2fa64ac48275f310977bb04
Jiacheng Huang <huangjiacheng0709@outlook.com>
2025-02-18 16:40:31 +0800

    Optimize 2D max pooling

--- max_pool2d.py
+++ max_pool2d.py
@@ -9,14 +9,27 @@ from ninetoothed import Symbol, Tensor
 
 
 def arrangement(input, output):
+    BLOCK_SIZE = Symbol("BLOCK_SIZE", meta=True)
+
     WINDOW_HEIGHT = Symbol("WINDOW_HEIGHT", constexpr=True)
     WINDOW_WIDTH = Symbol("WINDOW_WIDTH", constexpr=True)
 
-    return input.tile((1, 1, WINDOW_HEIGHT, WINDOW_WIDTH)), output.tile((1, 1, 1, 1))
+    input_arranged = input.tile((1, 1, WINDOW_HEIGHT, WINDOW_WIDTH))
+    input_arranged = input_arranged.ravel()
+    input_arranged = input_arranged.flatten(end_dim=4).flatten(start_dim=1)
+    input_arranged = input_arranged.tile((BLOCK_SIZE, -1))
+
+    output_arranged = output.tile((1, 1, 1, 1))
+    output_arranged = output_arranged.ravel()
+    output_arranged = output_arranged.flatten(end_dim=4).flatten(start_dim=1)
+    output_arranged = output_arranged.tile((BLOCK_SIZE, -1))
+    output_arranged.dtype = output_arranged.dtype.squeeze(1)
+
+    return input_arranged, output_arranged
 
 
 def application(input, output):
-    output = ntl.max(input)  # noqa: F841
+    output = ntl.max(input, axis=1)  # noqa: F841
 
 
 max_pool2d_kernel = ninetoothed.make(
